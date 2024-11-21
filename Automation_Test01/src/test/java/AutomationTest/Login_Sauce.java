96ac925d8177fb8d93090dd18d51d2dbaf83ba79
User <User@DESKTOP-5903S8A>
2024-11-21 21:13:43 +0600

    Commit Massage

---
+++ Automation_Test01/src/test/java/AutomationTest/Login_Sauce.java
@@ -0,0 +1,26 @@
+package AutomationTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login_Sauce {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.firefoxdriver().setup();
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+
+	}
+
+}
